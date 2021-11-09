9c277cff0d06f55808074c9b8189c3f66ae4013d
HaileyesusAK <hailejesus@gmail.com>
2021-11-09 14:46:56 +0100

    declare Assembler class

---
+++ C++/assembler/include/assembler.h
@@ -0,0 +1,8 @@
+#ifndef __ASSEMBLER_H__
+#define __ASSEMBLER_H__
+#include <string>
+
+class Assembler {
+};
+
+#endif
---
+++ C++/assembler/test/src/assembler_test.cpp
@@ -0,0 +1,7 @@
+#include "gmock/gmock.h"
+#include "assembler.h"
+
+
+TEST(HackAssembler, TranslatesAInstructionWithLiteral) {
+    Assembler assembler;
+}

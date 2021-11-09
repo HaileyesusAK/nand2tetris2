7b6ee0b2129ffa04f2789ac87b45ff174b16e8ba
HaileyesusAK <hailejesus@gmail.com>
2021-11-09 15:12:50 +0100

    assembler: refactor test

--- C++/assembler/test/src/assembler_test.cpp
+++ C++/assembler/test/src/assembler_test.cpp
@@ -1,15 +1,17 @@
 #include "gmock/gmock.h"
 #include "assembler.h"
 
+using namespace testing;
 
-TEST(HackAssembler, TranslatesAInstructionWithLiteral) {
-    Assembler assembler;
+class HackAssembler : public Test {
+    public:
+        Assembler assembler;
+};
 
-    ASSERT_THAT(assembler.translate("@0"), testing::Eq("0000000000000000"));
+TEST_F(HackAssembler, TranslatesAInstructionWithLiteral) {
+    ASSERT_THAT(assembler.translate("@0"), Eq("0000000000000000"));
 }
 
-TEST(HackAssembler, TranslatesAInstructionWithSymbol) {
-    Assembler assembler;
-
-    ASSERT_THAT(assembler.translate("@variable"), testing::Eq("0000000000010000"));
+TEST_F(HackAssembler, TranslatesAInstructionWithSymbol) {
+    ASSERT_THAT(assembler.translate("@variable"), Eq("0000000000010000"));
 }

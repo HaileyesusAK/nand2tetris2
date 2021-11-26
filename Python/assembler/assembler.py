7bb48c748d8069b9616ce8fe51416fcf825e41cd
HaileyesusAK <hailejesus@gmail.com>
2021-11-26 10:57:46 +0100

    assembler python: translate A instruction with symbol

--- Python/assembler/assembler.py
+++ Python/assembler/assembler.py
@@ -1,6 +1,25 @@
 class Assembler:
-    WSIZE = 16
+    WSIZE = 16      # word size in bits
+    VAR_MEM_BASE = 16 # base address of memory section for variables
+
+    def __init__(self):
+        self.symbol_table = {}
+        self.variables_count = 0
+
+    def _to_bin(self, value):
+        return f'{value:b}'.zfill(self.WSIZE)
 
     def translate(self, instruction):
-        literal = int(instruction[1:])
-        return f'{literal:b}'.zfill(self.WSIZE)
+        symbol = instruction[1:]
+        try:
+            value = int(symbol)
+        except ValueError:
+            value = self.symbol_table[symbol]
+
+        return self._to_bin(value)
+
+    def add_variable(self, var):
+        if var not in self.symbol_table:
+           self.symbol_table[var] = self.VAR_MEM_BASE + self.variables_count
+           self.variables_count += 1
+
--- Python/test/test_assembler.py
+++ Python/test/test_assembler.py
@@ -7,3 +7,9 @@ class TestAssembler(TestCase):
     def test_A_instruction_with_literal_translation(self):
         assembler = Assembler()
         self.assertEqual(assembler.translate("@33"), "0000000000100001")
+
+    def test_A_instruction_with_symbol_translation(self):
+        assembler = Assembler()
+        assembler.add_variable("m")
+        assembler.add_variable("n")
+        self.assertEqual(assembler.translate("@n"), "0000000000010001")
